d271d0e0c18e85f0649e34721a9128fd2d8d29b5
pagliarit <106833466+pagliarit@users.noreply.github.com>
2022-06-06 16:23:30 -0400

    Added lasthire context and mgr filter

--- NectarExport.sql
+++ NectarExport.sql
@@ -1,21 +1,19 @@
 --Query written by Tim Pagliari
 --pagliari.timothy@cuyahogabdd.org
---last edit 6/1/2022
+--last edit 6/6/2022 -- added 'LastHireDate' and filtered manager by primary email
 --Nectar Export file for SFTP flat file ingestion
 --Runs nightly in SSIS on Derby, discrepency export saves on \\Edam\group\reports\HR\
 --
---
-
 SELECT distinct 
---   , ei.EmployeeSSN as SSN 
+--   , ei.EmployeeSSN as xSSN
       rtrim(vp.FirstName) as first_name
      , rtrim(vp.LastName) as last_name
      , RTRIM(SHORTNM) as preferred_name
-     , REPLACE(CONVERT(varchar(11), originalhiredate, 106),' ','-') as hire_date
+     , REPLACE(CONVERT(varchar(11), LastHireDate, 106),' ','-') as hire_date
      , rtrim(isnull(rtrim(vem.EMailAddress),'')) as email
      , REPLACE(CONVERT(varchar(6),  vp.BirthDate, 106),' ','-') as birth_Date
-     ,Manemail.supemail manager_email
-	 ,rtrim(esi.EmpNo) as employee_id
+    ,Manemail.supemail manager_email
+   ,rtrim(esi.EmpNo) as employee_id
 --     ,esi.EffectiveDate
 
 FROM [tkcsdb].[dbo].[vHRL_Employeeinfo] ei
@@ -45,17 +43,17 @@ FROM [tkcsdb].[dbo].[vHRL_Employeeinfo] ei
             and vpc.customdatadefid = 1
        left join vEMAIL vem on vp.PersonIdNo = vem.PersonIdNo
                                   and vem.EmailPrimaryInd = 1  
-		
-		
-		
-		left outer join (select esi2.empno supnumber, vem2.EMailAddress supemail --generate dynamic manager email fields
-		from [tkcsdb].[dbo].[vHRL_Employeeinfo] ei2
-		left join (
+    
+    
+    
+    left outer join (select esi2.empno supnumber, vem2.EMailAddress supemail 
+    from [tkcsdb].[dbo].[vHRL_Employeeinfo] ei2
+    left join (
           SELECT PersonIdNo, PersonTaxIdNo, LastName, FirstName, MiddleName, BirthDate, MAX(PersonToEffectDate) 'EffectiveDate' FROM vPersons
              where PersonToEffectDate = '3000-01-01' GROUP BY PersonIdNo, PersonTaxIdNo, LastName, FirstName, MiddleName, BirthDate)  vp2 on ei2.EmployeeSSN = vp2.PersonTaxIdNo
-		left JOIN vHRL_EmploymentStatusInfo  esi2
+    left JOIN vHRL_EmploymentStatusInfo  esi2
           ON esi2.EmployeeSSN = vp2.PersonTaxIdNo
-		left join vEMAIL vem2 on vp2.PersonIDNo = vem2.PersonIdNo ) ManEmail on SUPERVISORNUM = ManEmail.supnumber --left join (SELECT rtrim(esi.EmpNo) as xempid, SUPERVISORFULLNAME supname ,SUPERVISORNUM	supid) managers on managers.xempid = manager_ID
+    left join vEMAIL vem2 on vp2.PersonIDNo = vem2.PersonIdNo and vem2.EmailPrimaryInd = 1  ) ManEmail on SUPERVISORNUM = ManEmail.supnumber --left join (SELECT rtrim(esi.EmpNo) as xempid, SUPERVISORFULLNAME supname ,SUPERVISORNUM  supid) managers on managers.xempid = manager_ID
 
     where  vps.EffectiveDate = (select MAX(vps2.Effectivedate)
                                 from vHRL_PayStatusInfo vps2
